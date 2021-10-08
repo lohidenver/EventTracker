cbb17bf08b6f734eb2191b2933e75e32df836fbe
lohidenver <broadcastdave@gmail.com>
2021-10-08 15:28:22 -0600

    project setup 2 configure project

---
+++ JPAgpk/src/main/java/com/skilldistillery/gpk/entities/Category.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.gpk.entities;
+
+public class Category {
+
+}
---
+++ JPAgpk/src/main/java/com/skilldistillery/gpk/entities/Product.java
@@ -0,0 +1,74 @@
+package com.skilldistillery.gpk.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+
+	public Product() {
+		super();
+	}
+
+	public Product(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return id == other.id && Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Product [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+	
+}
